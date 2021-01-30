7eaf718d3b453c7293d12d646d36dfe04c3cba66
ReadWrite <ReadWriteV@outlook.com>
2021-01-30 15:31:06 +0800

    update comment

--- include/front_driver/driver.h
+++ include/front_driver/driver.h
@@ -6,6 +6,10 @@
 
 namespace CPC
 {
+    /**
+     * @brief Driver to connect Scanner and Parser
+     * 
+     */
     class Driver
     {
     private:
@@ -14,19 +18,49 @@ namespace CPC
         std::string fileName;
 
     public:
+        /**
+         * @brief Construct a new Driver object
+         * 
+         */
         Driver() = delete;
+
+        /**
+         * @brief Construct a new Driver object
+         * 
+         * @param fileName Name of the file to scan and parse
+         */
         Driver(std::string fileName) : scanner(), parser(*this), fileName(fileName) {}
+
+        /**
+         * @brief Destroy the Driver object
+         * 
+         */
         ~Driver() {}
 
+        /**
+         * @brief Get the File Name
+         * 
+         * @return std::string File Name
+         */
         std::string getFileName() const
         {
             return fileName;
         }
+
+        /**
+         * @brief use Scanner to get next token for parser
+         * 
+         * @return Parser::symbol_type Next token
+         */
         Parser::symbol_type scan()
         {
             return scanner.nextToken();
         }
 
+        /**
+         * @brief parse the given file
+         * 
+         */
         void parse();
     };
 } // namespace CPC
